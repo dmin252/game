d29571b73330655825fdfc9065b8a8e8022cc8fc
dmin252 <smin@brooksschool.org>
2024-05-24 11:11:43 -0400

    Start of the Board Class
    Initial setup. Still has to finish makeBoard

---
+++ Board.java
@@ -0,0 +1,12 @@
+public class Board {
+    private ArrayList<Square> squares;
+    public Board() {
+        squares = new ArrayList<Square>();
+        makeBoard();
+    }
+
+    //populate the board
+    public void makeBoard() {
+        
+    }
+}
