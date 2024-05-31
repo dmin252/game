b39b60f22fff6ea44cf9ea4146d4b91d57be4d45
Seho Min <149593119+dmin252@users.noreply.github.com>
2024-05-31 02:42:56 +0000

    Implement correct railroad pricing

--- Railroad.java
+++ Railroad.java
@@ -5,8 +5,14 @@ public class Railroad extends Property {
     }
 
     public int getCost() {
-        
-        return 25;
+        int numRailroads = 0;
+        for (Property p : getOwner().getProperties()) {
+            if (p instanceof Railroad) {
+                numRailroads++;
+            }
+        }
+        int[] rents = { 0, 25, 50, 100, 200 };
+        return rents[numRailroads];
     }
 
 }
